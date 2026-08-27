1e2cc1dab93d38534a97d921b4066d387b92fb9c
Oleksandr Ratseburzhynskyi <ratseburzhynskyi@gmail.com>
2026-08-27 13:00:18 +0300

    add solution

---
+++ src/components/PeoplePage.tsx
@@ -0,0 +1,48 @@
+import { useState, useEffect } from 'react';
+import { Loader } from '../components/Loader';
+import { Person } from '../types';
+import { PeopleTable } from './PeopleTable';
+
+export const PeoplePage = () => {
+  const [people, setPeople] = useState<Person[]>([]);
+  const [hasError, setHasError] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('https://mate-academy.github.io/react_people-table/api/people.json')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error();
+        }
+
+        return response.json();
+      })
+      .then(data => setPeople(data))
+      .catch(() => setHasError(true))
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  return (
+    <>
+      <h1 className="title">People Page</h1>
+
+      <div className="block">
+        <div className="box table-container">
+          {isLoading && <Loader />}
+
+          {hasError && (
+            <p data-cy="peopleLoadingError" className="has-text-danger">
+              Something went wrong
+            </p>
+          )}
+
+          {!isLoading && !hasError && people.length === 0 && (
+            <p data-cy="noPeopleMessage">There are no people on the server</p>
+          )}
+
+          {!isLoading && !hasError && <PeopleTable people={people} />}
+        </div>
+      </div>
+    </>
+  );
+};
